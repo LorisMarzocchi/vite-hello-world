b3178300eba372b6201083eb872b8bea5ab44f45
LorisMarzocchi <ml77182a@gmail.com>
2023-05-17 17:01:44 +0200

    createindex

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+  import Ciao from './components/Ciao.vue';
+  
+  export default{
+
+    components: {
+      Ciao,
+    },
+  };
+
+</script>
+
+
+<template>
+  <div>
+    <Ciao />
+  </div>
+</template>
+
+
+<style scoped>
+
+  
+</style>
